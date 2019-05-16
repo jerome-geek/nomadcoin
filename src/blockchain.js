34993b6d91196f54683d78e69ced0bf2c8651894
Jerome <jerome@spinprotocol.io>
2019-05-17 00:28:46 +0900

    Create the Genesis Block

--- src/blockchain.js
+++ src/blockchain.js
@@ -7,3 +7,14 @@ class Block {
     this.data = data;
   }
 }
+
+const genesisBlock = new Block(
+  0,
+  '29B1A40E130E6DBDA288A538C8E99BAC1990452A60E2337D5E4A7FE52A5B7CAF',
+  null,
+  1558020374634,
+  'This is the genesis'
+);
+
+let blockchain = [genesisBlock];
+console.log('TCL: blockchain', blockchain);
